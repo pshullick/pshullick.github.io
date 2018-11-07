54f977f60eb6affa7c7c857bdc659fd09ffdbf12
Tricia Shullick <triciashullick@Tricias-MacBook-Pro.local>
2018-11-07 10:22:27 -0500

    Wed Nov  7 10:22:27 EST 2018

---
+++ dinosaurs/js/menushrink.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('shrink');
+  } else {
+    $('nav').removeClass('shrink');
+  }
+});
